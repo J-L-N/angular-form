8a236f6d8e941bb76cc9d2fe591ac3b0015e89d7
https://gitlab.isoad.isogmbh.de/jln <jeanlambert.niyigaba@iso-gruppe.com>
2024-11-15 17:25:38 +0100

    setting up Reactive form and form control

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,55 +1,20 @@
-import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
-import { EmailValidator, FormsModule, NgForm } from '@angular/forms';
-import { debounce, debounceTime } from 'rxjs';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  private form = viewChild.required<NgForm>('form');
-  private destroyRef = inject(DestroyRef);
+  form = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl('')
+  });
 
-  constructor() {
-    afterNextRender(() => {
-      const savedForm = window.localStorage.getItem('saved-login-form');
+  onSubmit() {
 
-      if (savedForm) {
-        const loadedFormData = JSON.parse(savedForm);
-        const savedemail = loadedFormData.email;
-        setTimeout(() => {
-          this.form().controls['email'].setValue(savedemail);
-        }, 1);
-      }
-
-      const subscription = this.form().valueChanges?.pipe(debounceTime(500)).subscribe({
-        next: (value) =>
-          window.localStorage.setItem(
-            'saved-login-form',
-            JSON.stringify({email: value.email})
-          ),
-      });
-
-      this.destroyRef.onDestroy(() => subscription?.unsubscribe());
-    });
-  }
-
-  onSubmit(formData: NgForm) {
-
-    if(formData.form.valid) {
-      return;
-    }
-
-
-    const enteredEmail = formData.form.value.email;
-    const enteredPassword = formData.form.value.password;
-
-    console.log(formData.form);
-    console.log(enteredEmail, enteredPassword);
-
-    formData.form.reset();
   }
 }
